536ad7cffe51aaebd28506b6ebfb74db84336131
ytradish <yellowtail.radish@gmail.com>
2020-04-07 11:59:52 +0900

    HTMLライターってキーボード文化なのかな

--- MyJSPractice/UsefulVisualStudioCode/js/main.js
+++ MyJSPractice/UsefulVisualStudioCode/js/main.js
@@ -1,7 +1,7 @@
 // querySelector = 指定したセレクターに一致する最初の要素を返す。
-const h1 = document.querySelector('h1');
+const h2 = document.querySelector('h2');
 
-h1.addEventListener('click', () => {
-    h1.textContent = 'changed';
+h2.addEventListener('click', () => {
+    h2.textContent = 'changed';
 });
 
